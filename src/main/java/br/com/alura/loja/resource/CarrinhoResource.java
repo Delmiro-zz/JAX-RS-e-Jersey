eac236b98de017c70d39686f81c05108f2a202b6
Diego Delmiro <diegoalbuquerque03@gmail.com>
2019-01-23 00:04:34 -0300

    Adicionando o status code ao método adicionar.

--- src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -1,15 +1,15 @@
 package br.com.alura.loja.resource;
 
+import java.net.URI;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
-import com.thoughtworks.xstream.XStream;
-
+import javax.ws.rs.core.Response;
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
+import com.thoughtworks.xstream.XStream;
 
 @Path("carrinhos")
 public class CarrinhoResource {
@@ -23,10 +23,11 @@ public class CarrinhoResource {
 	
 	@POST
 	@Produces(MediaType.APPLICATION_XML)
-	public String adiciona(String conteudo){
+	public Response adiciona(String conteudo){
 		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
 		new CarrinhoDAO().adiciona(carrinho);
-		return "<status>Sucesso</status>";
+		URI uri = URI.create("/carrinhos/"+ carrinho.getId());
+		return Response.created(uri).build();
 	}
 	
 }
