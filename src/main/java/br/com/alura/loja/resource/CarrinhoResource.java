27a2e8a3fc0d923bcc8582c0e9a7c8a26ada5ae8
Diego Delmiro <diegoalbuquerque03@gmail.com>
2019-01-23 00:09:22 -0300

    Ajuste.

--- src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -1,14 +1,18 @@
 package br.com.alura.loja.resource;
 
 import java.net.URI;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
+
 import com.thoughtworks.xstream.XStream;
 
 @Path("carrinhos")
@@ -22,7 +26,7 @@ public class CarrinhoResource {
 	}
 	
 	@POST
-	@Produces(MediaType.APPLICATION_XML)
+	@Consumes(MediaType.APPLICATION_XML)
 	public Response adiciona(String conteudo){
 		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
 		new CarrinhoDAO().adiciona(carrinho);
